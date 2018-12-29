906a4aa547b275228598683c6712cbd25b8fcfd7
samrg472 <samrg472@gmail.com>
2018-12-28 17:56:29 -0800

    Add support for typescript in the server

---
+++ server/index.ts
@@ -0,0 +1,44 @@
+import * as Koa from 'koa';
+import * as consola from 'consola';
+import { Nuxt, Builder } from 'nuxt';
+
+const app = new Koa();
+const host = process.env.HOST || '127.0.0.1';
+const port = Number.parseInt(process.env.PORT || '3000');
+
+// Import and Set Nuxt.js options
+let config = require('../nuxt.config.js');
+config.dev = !(app.env === 'production');
+
+async function start() {
+  // Instantiate nuxt.js
+  const nuxt = new Nuxt(config);
+
+  // Build in development
+  if (config.dev) {
+    const builder = new Builder(nuxt);
+    await builder.build();
+  }
+
+  app.use(ctx => {
+    ctx.status = 200;
+
+    return new Promise((resolve, reject) => {
+      ctx.res.on('close', resolve);
+      ctx.res.on('finish', resolve);
+      nuxt.render(ctx.req, ctx.res, promise => {
+        promise.then(resolve).catch(reject);
+      });
+    });
+  })
+
+  app.listen(port, host);
+  consola.ready({
+    message: `Server listening on http://${host}:${port}`,
+    badge: true
+  });
+}
+
+start().catch(e => {
+  consola.error('Startup error!\n', e);
+});
